5bf10cdf2fc6adfb310ae774f70f9db2f519e348
Jack Meyer <jackcmeyer@gmail.com>
2018-03-04 18:42:46 -0600

    tests for os data and http data routes

---
+++ test/routes/httpdata.routes.test.js
@@ -0,0 +1,34 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import * as apiUtil from '../test-utils/api.util';
+import server from '../globalSetupTeardown.test';
+
+chai.use(chaiHttp);
+const should = chai.should();
+const expect = chai.expect;
+const endpoint = '/httpdata';
+
+describe('Testing Tokens Routes', () => {
+  describe(`POST /httpdata`, () => {
+    it('should return a 401 if the token is not valid', (done) => {
+      chai.request(server)
+        .post(`${apiUtil.API}${endpoint}`)
+        .set('Authorization', apiUtil.bearerAuthorization('itsatrickgetanaxe'))
+        .send()
+        .end((err, res) => {
+          res.should.have.status(401);
+          done();
+        });
+    });
+    it('should return a 200 if the operation was successful', (done) => {
+      chai.request(server)
+        .post(`${apiUtil.API}${endpoint}`)
+        .set('Authorization', apiUtil.bearerAuthorization('t1'))
+        .send()
+        .end((err, res) => {
+          res.should.have.status(200);
+          done();
+        });
+    });
+  });
+});
